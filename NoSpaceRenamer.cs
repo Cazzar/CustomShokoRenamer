9fa2b6e2d8f5be7b11bb167c841cb35d3329a08e
Cayde Dixon <me@cazzar.net>
2018-02-18 15:50:12 +1100

    Example Renamers

---
+++ NoSpaceRenamer.cs
@@ -0,0 +1,27 @@
+﻿using Shoko.Models.Server;
+using Shoko.Server.Models;
+using Shoko.Server.Renamer;
+
+namespace Renamer.Cazzar
+{
+    [Renamer("NoSpaces", Description = "Renamer that doesn't use spaces")]
+    class NoSpaceRenamer : IRenamer
+    {
+        private readonly LegacyRenamer script;
+
+        public NoSpaceRenamer(RenameScript script)
+        {
+            this.script = new LegacyRenamer(script);
+        }
+
+        public string GetFileName(SVR_VideoLocal_Place place) => script.GetFileName(place);
+
+        public string GetFileName(SVR_VideoLocal video) => script.GetFileName(video);
+
+        public (ImportFolder dest, string folder) GetDestinationFolder(SVR_VideoLocal_Place video)
+        {
+            var (dest, folder) = script.GetDestinationFolder(video);
+            return (dest, folder.Replace(' ', '_'));
+        }
+    }
+}
