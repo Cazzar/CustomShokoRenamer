f400a1fc8ab1a7b050bc3d7770f8ffe814c1d38a
da3dsoul <da3dsoul@gmail.com>
2024-09-16 17:02:07 -0400

    Update MyRenamer to use Plugin Abstractions

--- InfluxDBPlugin.cs
+++ InfluxDBPlugin.cs
@@ -3,47 +3,52 @@ using System.Threading.Tasks;
 using InfluxDB.Client;
 using InfluxDB.Client.Api.Domain;
 using Renamer.Cazzar.Influx;
-using Shoko.Server;
-using Shoko.Server.Commands;
-using Shoko.Server.Plugin;
+using Shoko.Plugin.Abstractions;
 
-namespace Renamer.Cazzar
+namespace Renamer.Cazzar;
+
+public class InfluxDbPlugin : IPlugin
 {
-    public class InfluxDbPlugin : IPlugin
+    private readonly IShokoEventHandler _eventHandler;
+
+    private InfluxDBClient _influxDb = null;
+
+    public InfluxDbPlugin(IShokoEventHandler eventHandler)
     {
-
-        private InfluxDBClient _influxDb = null;
-
-        public string Name => "InfluxDB exporter";
-
-        public void Load()
-        {
-            CheckInflux().ConfigureAwait(false).GetAwaiter().GetResult();
-            ShokoService.CmdProcessorGeneral.OnQueueCountChangedEvent +=  e => QueueUpdate(e, "general");
-            ShokoService.CmdProcessorHasher.OnQueueCountChangedEvent += e => QueueUpdate(e, "hasher");
-            ShokoService.CmdProcessorImages.OnQueueCountChangedEvent += e => QueueUpdate(e, "images");
-        }
-
-        private async Task CheckInflux()
-        {
-            if (this._influxDb != null)
-            {
-                var health = await this._influxDb.HealthAsync();
-                if (health.Status == HealthCheck.StatusEnum.Pass) return;
-            }
-            this._influxDb = InfluxDBClientFactory.CreateV1("http://192.168.1.3:8086", "", new char[0], "shoko", "autogen");
-        }
-
-        private async void QueueUpdate(QueueCountEventArgs e, string queue)
-        {
-            await CheckInflux();
-            using var db = _influxDb.GetWriteApi();
-            var datapoint = new DataPoint()
-            {
-                Count = e.QueueCount,
-                Queue =  queue
-            };
-            db.WriteMeasurement("shoko", "default", WritePrecision.Ms, datapoint);
-        }
+        _eventHandler = eventHandler;
     }
+
+    public void OnSettingsLoaded(IPluginSettings settings)
+    {
+    }
+
+    public string Name => "InfluxDB exporter";
+
+    public void Load()
+    {
+        CheckInflux().ConfigureAwait(false).GetAwaiter().GetResult();
+        // TODO Add events and redesign graphs for new queue
+    }
+
+    private async Task CheckInflux()
+    {
+        if (this._influxDb != null)
+        {
+            var health = await this._influxDb.HealthAsync();
+            if (health.Status == HealthCheck.StatusEnum.Pass) return;
+        }
+        this._influxDb = InfluxDBClientFactory.CreateV1("http://192.168.1.3:8086", "", new char[0], "shoko", "autogen");
+    }
+
+    /*private async void QueueUpdate(QueueCountEventArgs e, string queue)
+    {
+        await CheckInflux();
+        using var db = _influxDb.GetWriteApi();
+        var datapoint = new DataPoint()
+        {
+            Count = e.QueueCount,
+            Queue =  queue
+        };
+        db.WriteMeasurement("shoko", "default", WritePrecision.Ms, datapoint);
+    }*/
 }
